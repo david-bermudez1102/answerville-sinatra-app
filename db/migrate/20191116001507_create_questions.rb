41e83a7f28f0ac2f939df4cb3e7cbf5d4b08c73a
Juan D. Bermudez <juan.bermudez1102@outlook.com>
2019-11-21 20:14:38 -0500

    Added column 'answered' in questions, if the question is marked as anwered

--- db/migrate/20191116001507_create_questions.rb
+++ db/migrate/20191116001507_create_questions.rb
@@ -2,6 +2,7 @@ class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
       t.text :content
+      t.boolean :answered
       t.integer :user_id
       t.timestamps
     end
